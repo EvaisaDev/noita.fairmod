3f40ac83a7be2192bf6ad938fe5c1eaedf4bcee5
Mir Sole <zburn101@gmail.com>
2024-10-08 23:52:59 +1100

    mimics spawn 1 in 75, assuming they CAN spawn bc script wont run >:(((

--- files/entities/animals/hm_portal_mimic/do_i_spawn.lua
+++ files/entities/animals/hm_portal_mimic/do_i_spawn.lua
@@ -5,20 +5,20 @@ local x, y = EntityGetTransform(portal)
 
 SetRandomSeed( x, y + tonumber(StatsGetValue("world_seed")))
 
-local doidoit = Random(1,3)
+local doidoit = Random(1,75)
 
---[[if doidoit == 2 then
+if doidoit == 2 then
     local comps = EntityGetAllComponents(portal)
     for i,v in ipairs(comps) do
         EntitySetComponentIsEnabled(portal, v, false)
     end
     EntityLoad("data/entities/animals/noita.fairmod_hm_portal_mimic.xml", x, y)
     EntityKill(portal)
-end]]
+end
 
-local comps = EntityGetAllComponents(portal)
+--[[local comps = EntityGetAllComponents(portal)
 for i,v in ipairs(comps) do
     EntitySetComponentIsEnabled(portal, v, false)
 end
 EntityLoad("data/entities/animals/noita.fairmod_hm_portal_mimic.xml", x, y)
-EntityKill(portal)
+EntityKill(portal)]]
