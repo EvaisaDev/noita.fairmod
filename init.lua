60f6313a114ebd1ee0e6839907226f6ca8bfeeaf
Human <54731850+Ramiels@users.noreply.github.com>
2024-10-08 07:00:36 -0400

    stuff

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 local fuckedupenemies = dofile("mods/noita.fairmod/files/scripts/fuckedupenemies.lua")
+local heartattack = dofile("mods/noita.fairmod/files/scripts/heartattack.lua")
+
+
+
+dofile_once("mods/noita.fairmod/files/scripts/coveryourselfinoil.lua")
+
+
+function OnPlayerSpawned()
+	heartattack.OnPlayerSpawned()
+end
 
 function OnWorldPreUpdate()
 	fuckedupenemies.OnWorldPreUpdate()
 end
+
+
+-- Copi was here
