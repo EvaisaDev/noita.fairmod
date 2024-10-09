780530a68677e95594125176434c3a737754f130
Evaisa <evagiacosa1@gmail.com>
2024-10-09 22:36:41 +0200

    better snail, and bananas
    snail now teleports more consistently, is respawned when it disappears, and stuff.
    Also added more stalactite triggers

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ dofile_once("mods/noita.fairmod/files/content/hm_portal_mimic/init.lua")
 dofile_once("mods/noita.fairmod/files/content/fungal_shift/fix_nolla_tags.lua")
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/noita.fairmod/files/content/rework_spells/rework_spells.lua")
 ModLuaFileAppend("data/scripts/magic/fungal_shift.lua", "mods/noita.fairmod/files/content/fungal_shift/append.lua")
+ModMaterialsFileAdd("mods/noita.fairmod/files/content/gold_bananas/materials.xml")
 
 --- I hate doing things without a hook
 function OnModPostInit()
@@ -56,8 +57,6 @@ function OnPlayerSpawned(player)
 
 	heartattack.OnPlayerSpawned(player)
 
-	local _, snail_x, snail_y = RaytracePlatforms(x - 100, y - 100, x - 100, y + 500)
-	EntityLoad("mods/noita.fairmod/files/content/immortal_snail/entities/snail.xml", snail_x, snail_y)
 	crits.OnPlayerSpawned(player)
 
 
@@ -74,6 +73,7 @@ ModRegisterAudioEventMappings("mods/noita.fairmod/GUIDs.txt")
 function OnWorldPreUpdate()
 	if GameGetFrameNum() % 30 == 0 then
 		fuckedupenemies:OnWorldPreUpdate()
+		dofile("mods/noita.fairmod/files/content/immortal_snail/scripts/spawn_snail.lua")
 	end
 	nukes.OnWorldPreUpdate();
 	input_delay.OnWorldPreUpdate()
