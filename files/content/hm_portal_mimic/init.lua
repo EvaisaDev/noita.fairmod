10d0f345ae08a7c1724eef31e9a02677bebff9bb
Mir Sole <zburn101@gmail.com>
2024-10-09 19:55:57 +1100

    moved hm portal mimic content to its own folder

--- files/content/hm_portal_mimic/init.lua
+++ files/content/hm_portal_mimic/init.lua
@@ -1,4 +1,4 @@
-local nxml = dofile_once("mods/noita.fairmod/lib/nxml.lua")
+local nxml = dofile_once("mods/noita.fairmod/files/lib/nxml.lua")
 
 local xml = nxml.parse(ModTextFileGetContent("data/entities/buildings/teleport_liquid_powered.xml"))
 xml:add_child(nxml.parse([[
@@ -6,7 +6,7 @@ xml:add_child(nxml.parse([[
         _tags="mimicspawner"
         _enabled="1" 
         execute_on_added="1"
-        script_source_file="mods/noita.fairmod/files/entities/animals/hm_portal_mimic/do_i_spawn.lua"
+        script_source_file="mods/noita.fairmod/files/content/hm_portal_mimic/do_i_spawn.lua"
         >
     </LuaComponent>
 ]]))
--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ local tm_trainer = dofile("mods/noita.fairmod/files/content/tmtrainer/init.lua")
 local crits = dofile("mods/noita.fairmod/files/content/crits/init.lua")
 
 dofile_once("mods/noita.fairmod/files/scripts/coveryourselfinoil.lua")
-dofile_once("mods/noita.fairmod/files/scripts/hm_portal_mimic.lua")
+dofile_once("mods/noita.fairmod/files/content/hm_portal_mimic/init.lua")
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/noita.fairmod/files/scripts/rework_spells.lua")
 
 
