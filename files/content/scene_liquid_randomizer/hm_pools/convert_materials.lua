ecc74070f6e726b21212751874c245c72faac403
Evaisa <evagiacosa1@gmail.com>
2024-10-14 14:56:34 +0200

    fix seed warnings

--- files/content/scene_liquid_randomizer/hm_pools/convert_materials.lua
+++ files/content/scene_liquid_randomizer/hm_pools/convert_materials.lua
@@ -1,13 +1,12 @@
 local water = CellFactory_GetType("water")
 
 dofile("mods/noita.fairmod/files/scripts/utils/utilities.lua")
-
-local liquids = MaterialsFilter(CellFactory_GetAllLiquids(false, false) or {})
-
 local entity_id = GetUpdatedEntityID()
 local x, y = EntityGetTransform(entity_id)
 
 SetRandomSeed(x, y)
+
+local liquids = MaterialsFilter(CellFactory_GetAllLiquids(false, false) or {})
 
 local random_liquid = CellFactory_GetType(liquids[Random(1, #liquids)])
 
