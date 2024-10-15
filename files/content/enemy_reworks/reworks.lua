264852a07995169930f629b43136c704673f13b2
Lamia <xaxnas@gmail.com>
2024-10-15 19:26:33 +0100

    mountain, fish

--- files/content/enemy_reworks/reworks.lua
+++ files/content/enemy_reworks/reworks.lua
@@ -1,5 +1,6 @@
 dofile_once("mods/noita.fairmod/files/content/enemy_reworks/hamis_reworked/hamis_reworked.lua")
 dofile_once("mods/noita.fairmod/files/content/enemy_reworks/firemage/replace_water.lua")
+dofile_once("mods/noita.fairmod/files/content/enemy_reworks/fish/regular_fish.lua")
 
 local append_after = {
 	["data/scripts/animals/giantshooter_death.lua"] = "mods/noita.fairmod/files/content/enemy_reworks/giant_shooter/death_script.lua",
---
+++ files/content/surface_bad/init.lua
@@ -0,0 +1,5 @@
+ModLuaFileAppend("data/scripts/biomes/mountain/mountain_top.lua", "mods/noita.fairmod/files/content/surface_bad/mountain_append.lua")
+ModLuaFileAppend("data/scripts/biomes/mountain_lake.lua", "mods/noita.fairmod/files/content/surface_bad/mountain_lake_append.lua")
+
+
+BiomeMaterialSetValue("data/biome/hills.xml", "soil", "material_name", "lava")
