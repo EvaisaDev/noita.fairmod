dd447a49b7ad7e4d7feab74d02d5872e58f64049
Evaisa <evagiacosa1@gmail.com>
2024-10-15 20:15:33 +0200

    Update damage.lua

--- files/content/crits/damage.lua
+++ files/content/crits/damage.lua
@@ -2,7 +2,7 @@
 function damage_about_to_be_received(damage, x, y, entity_thats_responsible, critical_hit_chance)
 	local crit_mult = 1
 	SetRandomSeed((x * GameGetFrameNum()) % 25781423, y + GameGetFrameNum())
-	while Random(1, 10) == 1 do
+	while Random(1, 20) == 1 do
 		--GamePrint("Got Crit!")
 		crit_mult = crit_mult * 5
 	end
