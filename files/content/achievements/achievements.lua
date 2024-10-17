916c5c22c05d4a1b2d8abbea20abf0ea3c8d38ea
Evaisa <evagiacosa1@gmail.com>
2024-10-17 12:48:01 +0200

    stuff

--- files/content/achievements/achievements.lua
+++ files/content/achievements/achievements.lua
@@ -2,7 +2,7 @@ achievements = {
 	{
 		name = "Shitted",
 		description = "You shitted and farted!!!",
-		icon = nil,
+		icon = "mods/noita.fairmod/files/content/achievements/icons/shitted.png",
 		flag = "achievement_shitted",
 		background = "mods/noita.fairmod/files/content/achievements/backgrounds/background_small.png",
 		unlock = function()
