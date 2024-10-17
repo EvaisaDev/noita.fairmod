c9ea47318694465775d5806d367ea2151ba54b53
inferknob <137335926+inferknob@users.noreply.github.com>
2024-10-16 20:20:41 -0400

    Update init.lua

--- files/content/legosfolder/init.lua
+++ files/content/legosfolder/init.lua
@@ -1,9 +1,1 @@
 ModMaterialsFileAdd("mods/noita.fairmod/files/content/legosfolder/legos.xml")
-
-local M = {}
-
-function M.OnPlayerSpawned(player_entity)
-  EntitySetDamageFromMaterial(player_entity, "mat_legos", 0.004)
-end
-
-return M
