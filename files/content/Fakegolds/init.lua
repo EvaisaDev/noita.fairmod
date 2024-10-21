f9cbfab18289d6171a75f9e6fb419d513b6eb681
Evaisa <mail@evaisa.dev>
2024-10-21 10:35:41 +0200

    Update init.lua

--- files/content/Fakegolds/init.lua
+++ files/content/Fakegolds/init.lua
@@ -2,22 +2,22 @@ local content = ModTextFileGetContent("data/biome/gold.xml")
 local thegold = {}
 
 local mats = {
-    "beer",
-    "poo",
-    "gold_molten",
-    "gold_radioactive",
-    "urine",
-    "alcohol",
-    "fungi",
-    "magic_gas_hp_regeneration",
-    "milk",
-    "molut",
-    "rock_hard_border",
-    "templeslab_static",
+	"beer",
+	"poo",
+	"gold_molten",
+	"gold_radioactive",
+	"urine",
+	"alcohol",
+	"fungi",
+	"magic_gas_hp_regeneration",
+	"milk",
+	"molut",
+	"rock_hard_border",
+	"templeslab_static",
 }
 function thegold.OnMagicNumbersAndWorldSeedInitialized()
-    content = content:gsub("\"gold\"", "\""..mats[Random(1, #mats)].."\"")
-    ModTextFileSetContent("data/biome/gold.xml", content)
+	content = content:gsub("\"gold\"", "\""..mats[Random(1, #mats)].."\"")
+	ModTextFileSetContent("data/biome/gold.xml", content)
 end
 
 return thegold
