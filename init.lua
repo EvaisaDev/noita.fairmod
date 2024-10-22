fa3a1ad213abe41ea653a12f50051f5da11c9250
Nathan <nathan.titirangi@gmail.com>
2024-10-22 22:08:56 +1300

    banana and update nxml

--- init.lua
+++ init.lua
@@ -52,9 +52,9 @@ dofile_once("mods/noita.fairmod/files/content/permanent_self_damage/init.lua")
 dofile_once("mods/noita.fairmod/files/content/mask_box/init.lua")
 dofile_once("mods/noita.fairmod/files/content/bananapeel/init.lua")
 dofile_once("mods/noita.fairmod/files/content/spooky_skeleton/init.lua")
+dofile_once("mods/noita.fairmod/files/content/gold_bananas/init.lua")
 
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/noita.fairmod/files/content/rework_spells/rework_spells.lua")
-ModMaterialsFileAdd("mods/noita.fairmod/files/content/gold_bananas/materials.xml")
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/noita.fairmod/files/content/minus_life/perk.lua")
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/noita.fairmod/files/content/mon_wands/perk.lua")
 ModLuaFileAppend(
