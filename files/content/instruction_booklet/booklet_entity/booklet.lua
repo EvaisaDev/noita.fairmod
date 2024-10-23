42ece96f57a3ed74858407bf57477254d8aa072a
Lamia <xaxnas@gmail.com>
2024-10-23 20:58:53 +0100

    debug

--- files/content/instruction_booklet/booklet_entity/booklet.lua
+++ files/content/instruction_booklet/booklet_entity/booklet.lua
@@ -1,7 +1,6 @@
 local ui = dofile_once("mods/noita.fairmod/files/content/instruction_booklet/gui.lua") --- @class instruction_booklet_gui
 
 function enabled_changed(entity_id, is_enabled)
-	print("\n\n\n\n" .. tostring(is_enabled) .. "\n\n\n\n")
 	if is_enabled then
 		ui.gui = GuiCreate()
 	else
