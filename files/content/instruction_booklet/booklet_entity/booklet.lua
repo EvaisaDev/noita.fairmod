a85d5d61af46b680a0b81505d84aff76fc35edbd
Lamia <xaxnas@gmail.com>
2024-10-23 19:41:43 +0100

    why

--- files/content/instruction_booklet/booklet_entity/booklet.lua
+++ files/content/instruction_booklet/booklet_entity/booklet.lua
@@ -1,11 +1,14 @@
 local ui = dofile_once("mods/noita.fairmod/files/content/instruction_booklet/gui.lua") --- @class instruction_booklet_gui
 
-ui:update()
-
 function enabled_changed(entity_id, is_enabled)
+	print("\n\n\n\n" .. tostring(is_enabled) .. "\n\n\n\n")
 	if is_enabled then
 		ui.gui = GuiCreate()
 	else
 		GuiDestroy(ui.gui)
 	end
+end
+
+function wake_up_waiting_threads()
+	ui:update()
 end
