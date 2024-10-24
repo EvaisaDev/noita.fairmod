567363c886e5b89d6e8ab553af809e96b1ca37c3
Evaisa <evagiacosa1@gmail.com>
2024-10-24 11:08:13 +0200

    destroy projectiles on trigger

--- files/content/stalactite/scripts/trigger.lua
+++ files/content/stalactite/scripts/trigger.lua
@@ -2,7 +2,7 @@ dofile_once("data/scripts/lib/utilities.lua")
 
 local function get_chance(colliding_entity_id)
 	if EntityHasTag(colliding_entity_id, "player_projectile") then
-		return 80
+		return 50
 	elseif EntityHasTag(colliding_entity_id, "enemy") then
 		return 4
 	end
@@ -30,25 +30,33 @@ function should_collapse(colliding_entity_id)
 end
 
 function collision_trigger(colliding_entity_id)
-	if should_collapse(colliding_entity_id) == false then
-		return
+
+	if should_collapse(colliding_entity_id)then
+		local entity_id = GetUpdatedEntityID()
+		local x, y = EntityGetTransform(entity_id)
+
+		local storage = get_variable_storage_component(entity_id, "projectile")
+		local projectile = ComponentGetValue2(storage, "value_string")
+
+		local entity = shoot_projectile(entity_id, projectile, x, y + 3, 0, 300, false)
+
+		EntityLoad("mods/noita.fairmod/files/content/stalactite/entities/misc/poof.xml", x, y)
+
+		local physics_body_component = EntityGetFirstComponent(entity, "PhysicsBodyComponent")
+		if physics_body_component ~= nil then
+			local x, y, angle, vel_x, vel_y, angular_vel = PhysicsComponentGetTransform(physics_body_component)
+			PhysicsComponentSetTransform(physics_body_component, x, y, angle, 0, 25, angular_vel)
+		end
+
+		EntityKill(entity_id)
 	end
 
-	local entity_id = GetUpdatedEntityID()
-	local x, y = EntityGetTransform(entity_id)
+	local is_projectile = EntityHasTag(colliding_entity_id, "projectile") or EntityHasTag(colliding_entity_id, "player_projectile")
 
-	local storage = get_variable_storage_component(entity_id, "projectile")
-	local projectile = ComponentGetValue2(storage, "value_string")
-
-	local entity = shoot_projectile(entity_id, projectile, x, y + 3, 0, 300, false)
-
-	EntityLoad("mods/noita.fairmod/files/content/stalactite/entities/misc/poof.xml", x, y)
-
-	local physics_body_component = EntityGetFirstComponent(entity, "PhysicsBodyComponent")
-	if physics_body_component ~= nil then
-		local x, y, angle, vel_x, vel_y, angular_vel = PhysicsComponentGetTransform(physics_body_component)
-		PhysicsComponentSetTransform(physics_body_component, x, y, angle, 0, 25, angular_vel)
+	if(is_projectile)then
+		local projectile_component = EntityGetFirstComponent(colliding_entity_id, "ProjectileComponent")
+		if(projectile_component ~= nil)then
+			ComponentSetValue2(projectile_component, "lifetime", 1)
+		end
 	end
-
-	EntityKill(entity_id)
 end
