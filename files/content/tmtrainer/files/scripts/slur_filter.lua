0ee09f1b3b74e517fdc6f87f1740f8e83ee2354f
Evaisa <evagiacosa1@gmail.com>
2024-10-27 15:59:52 +0100

    base64 encode the bad words
    because people didn't like having the plaintext list!

--- files/content/tmtrainer/files/scripts/slur_filter.lua
+++ files/content/tmtrainer/files/scripts/slur_filter.lua
@@ -1,51 +1,30 @@
 local slurs = {
-	"beaners",
-	"beaner",
-	"bimbo",
-	"coon",
-	"coons",
-	"cunt",
-	"cunts",
-	"darkie",
-	"darkies",
-	"fag",
-	"fags",
-	"faggot",
-	"faggots",
-	"hooker",
-	"kike",
-	"kikes",
-	"nazi",
-	"nazis",
-	"neonazi",
-	"neonazis",
-	"negro",
-	"negros",
-	"nigga",
-	"niggas",
-	"nigger",
-	"niggers",
-	"paki",
-	"pakis",
-	"raghead",
-	"ragheads",
-	"shemale",
-	"shemales",
-	"slut",
-	"sluts",
-	"spic",
-	"spics",
-	"swastika",
-	"towelhead",
-	"towelheads",
-	"tranny",
-	"trannys",
-	"trannies",
-	"twink",
-	"twinks",
-	"wetback",
-	"wetbacks"
 }
+
+local content = ModTextFileGetContent("mods/noita.fairmod/files/content/tmtrainer/files/scripts/bad_words.txt")
+-- base64 decode
+local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function base64_decode(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r, f = '', (b:find(x) - 1)
+        for i = 6, 1, -1 do r = r .. (f % 2^i - f % 2^(i - 1) > 0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c = 0
+        for i = 1, 8 do c = c + (x:sub(i, i) == '1' and 2^(8 - i) or 0) end
+        return string.char(c)
+    end))
+end
+
+content = base64_decode(content)
+
+for line in content:gmatch("[^\r\n]+") do
+	table.insert(slurs, line)
+end
+
 
 local module = {}
 
