dc0772039de42c94269cf0a426e0627ffb98309a
Lamia <xaxnas@gmail.com>
2024-10-30 19:53:16 +0000

    reset dmca warn flag on reset

--- files/content/reset_progress/init.lua
+++ files/content/reset_progress/init.lua
@@ -7,6 +7,8 @@ for i = 1, #achievements do
 	local flag = "fairmod_" .. achievement.flag or ("achievement_" .. achievement.name)
 	RemoveFlagPersistent(flag)
 end
+
+RemoveFlagPersistent("fairmod_dmca_warning_shown")
 ModSettingRemove("fairmod_win_count")
 ModSettingRemove("fairmod.death_locations")
 ModSettingRemove("noita.fairmod.reset_progress")
