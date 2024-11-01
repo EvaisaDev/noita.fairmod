1a8a1c146bde43727010cca90c02a11fa6872132
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-11-01 17:51:24 +0000

    added new hammi tips
    also added background hint under blacklights

--- files/content/backrooms/entities/radio.lua
+++ files/content/backrooms/entities/radio.lua
@@ -10,6 +10,8 @@ function interacting(entity_who_interacted, entity_interacted, interactable_name
 	if radio_is_on then
 		EntitySetComponentsWithTagEnabled(entity, "radio_on", true)
 		EntitySetComponentsWithTagEnabled(entity, "radio_off", false)
+		local audiocomp = EntityGetFirstComponent(entity, "AudioLoopComponent")
+		if audiocomp then ComponentSetValue2(audiocomp, "event_name", "radio/" .. (Random(1, 10) == 9 and "ill_see_you_when_i_see_you" or "loop")) end
 		GlobalsSetValue("radios_activated", tostring(tonumber(GlobalsGetValue("radios_activated", "0")) + 1))
 	else
 		EntitySetComponentsWithTagEnabled(entity, "radio_on", false)
