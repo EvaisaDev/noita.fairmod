4e3dbb2506b720bae1e922bdaf807a0243c72d5a
Evaisa <evagiacosa1@gmail.com>
2024-11-01 20:02:14 +0100

    ambrosia tomfoolery

---
+++ files/content/worse_materials/append_status_list.lua
@@ -0,0 +1,7 @@
+table.insert(status_effects, {
+	id="PROTECTION_NONE",
+	ui_name="$status_protection_none",
+	ui_description="$statusdesc_protection_none",
+	ui_icon="data/ui_gfx/status_indicators/protection_all.png",
+	effect_entity="mods/noita.fairmod/files/content/worse_materials/effect_protection_none.xml",
+})
