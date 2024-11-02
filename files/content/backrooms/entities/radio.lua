b71fe40b90c191cb7f22ab69794a5d35f29231fb
Evaisa <evagiacosa1@gmail.com>
2024-11-02 20:24:34 +0100

    fix backrooms radio counter

--- files/content/backrooms/entities/radio.lua
+++ files/content/backrooms/entities/radio.lua
@@ -1,13 +1,10 @@
-radio_is_on = radio_is_on or false
+
 
 function interacting(entity_who_interacted, entity_interacted, interactable_name)
-	radio_is_on = not radio_is_on
+	local radio_is_on = EntityGetFirstComponent(entity_interacted, "AudioLoopComponent", "radio_on") ~= nil
 	local entity = GetUpdatedEntityID()
 
-	local audio_loop_off = EntityGetFirstComponentIncludingDisabled(entity, "AudioLoopComponent", "radio_off")
-	local audio_loop_on = EntityGetFirstComponentIncludingDisabled(entity, "AudioLoopComponent", "radio_on")
-
-	if radio_is_on then
+	if not radio_is_on then
 		EntitySetComponentsWithTagEnabled(entity, "radio_on", true)
 		EntitySetComponentsWithTagEnabled(entity, "radio_off", false)
 		local audiocomp = EntityGetFirstComponent(entity, "AudioLoopComponent")
