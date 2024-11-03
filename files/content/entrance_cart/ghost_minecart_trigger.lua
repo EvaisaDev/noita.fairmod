8c9137628eb5d8ccfdff8fff991f0c536bd40899
Adam Heinermann <aheinerm@gmail.com>
2024-11-03 01:08:31 -0700

    Minecart troll is 100x more dangerous

--- files/content/entrance_cart/ghost_minecart_trigger.lua
+++ files/content/entrance_cart/ghost_minecart_trigger.lua
@@ -1,5 +1,12 @@
 function collision_trigger(colliding_entity_id)
 	local x, y = EntityGetTransform(GetUpdatedEntityID())
 
-	EntityLoad("data/entities/props/physics/minecart.xml", x - 210, y - 240)
+	local col_x, col_y = EntityGetTransform(colliding_entity_id)
+	local rnd = ProceduralRandomi(col_x, col_y + GameGetFrameNum(), 1, 50)
+
+	if rnd == 1 then
+		EntityLoad("data/entities/projectiles/bomb_cart.xml", x - 210, y - 240)
+	else
+		EntityLoad("data/entities/props/physics/minecart.xml", x - 210, y - 240)
+	end
 end
