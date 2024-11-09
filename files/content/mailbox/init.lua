0eb8563f97e70c9abe82cdae4062260b58578051
Human <54731850+Ramiels@users.noreply.github.com>
2024-11-08 21:02:01 -0500

    fix acheevo n mail

--- files/content/mailbox/init.lua
+++ files/content/mailbox/init.lua
@@ -6,6 +6,13 @@ module.spawn = function(x, y)
 		ModSettingSet("noita.fairmod.mail", (ModSettingGet("noita.fairmod.mail") or "") .. "welcome,")
 	end
 
+	if Random(1,100)==1 then
+		if(not HasFlagPersistent("fairmod_gamebro_letter"))then
+			AddFlagPersistent("fairmod_gamebro_letter")
+			ModSettingSet("noita.fairmod.mail", (ModSettingGet("noita.fairmod.mail") or "") .. "gamebro,")
+		end
+	end
+
 	EntityLoad("mods/noita.fairmod/files/content/mailbox/mailbox.xml", x + 110, y - 15)
 end
 
