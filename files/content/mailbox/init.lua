9d0e0101850ccab63d9a66ec9c1ba08c0ca50a48
Human <54731850+Ramiels@users.noreply.github.com>
2024-11-08 21:07:02 -0500

    gamebro roll

--- files/content/mailbox/init.lua
+++ files/content/mailbox/init.lua
@@ -1,17 +1,19 @@
 local module = {}
 
 module.spawn = function(x, y)
-	if(not HasFlagPersistent("fairmod_first_time_mailbox"))then
-		AddFlagPersistent("fairmod_first_time_mailbox")
-		ModSettingSet("noita.fairmod.mail", (ModSettingGet("noita.fairmod.mail") or "") .. "welcome,")
-	end
 
-	if Random(1,100)==1 then
+	if Random(1,100)==1 and HasFlagPersistent("fairmod_first_time_mailbox") then
 		if(not HasFlagPersistent("fairmod_gamebro_letter"))then
 			AddFlagPersistent("fairmod_gamebro_letter")
 			ModSettingSet("noita.fairmod.mail", (ModSettingGet("noita.fairmod.mail") or "") .. "gamebro,")
 		end
 	end
+
+	if(not HasFlagPersistent("fairmod_first_time_mailbox"))then
+		AddFlagPersistent("fairmod_first_time_mailbox")
+		ModSettingSet("noita.fairmod.mail", (ModSettingGet("noita.fairmod.mail") or "") .. "welcome,")
+	end
+
 
 	EntityLoad("mods/noita.fairmod/files/content/mailbox/mailbox.xml", x + 110, y - 15)
 end
