60aa66294aab1385296af8d9bfd91340bf5cc57d
Evaisa <evagiacosa1@gmail.com>
2025-03-09 15:36:27 +0100

    Update init.lua

--- files/content/permanent_self_damage/init.lua
+++ files/content/permanent_self_damage/init.lua
@@ -6,7 +6,7 @@ do
 	local xml = nxml.parse(content)
 	xml:add_child(nxml.parse([[
         <LuaComponent
-            script_shot="data/scripts/status_effects/twitchy_shot.lua"
+            script_shot="mods/noita.fairmod/files/content/permanent_self_damage/twitchy_shot.lua"
             execute_every_n_frame="-1"
             >
         </LuaComponent>
