e62d4929ba3d3306a9c7817ef0c69569b3bb2b8e
Lamia <xaxnas@gmail.com>
2025-03-15 22:19:33 +0000

    some changes to materials

---
+++ files/content/new_materium/spawners/tnt.lua
@@ -0,0 +1,23 @@
+local files = {
+	"data/entities/projectiles/tnt.xml",
+	"mods/noita.fairmod/files/content/new_spells/bad_bombs/tnt.xml",
+	"mods/noita.fairmod/files/content/new_spells/bad_bombs/tnt_long.xml",
+	"mods/noita.fairmod/files/content/new_spells/bad_bombs/tnt_beeg.xml",
+}
+
+local max = 40
+local radius = 20
+
+local entity_id = GetUpdatedEntityID()
+local x, y = EntityGetTransform(entity_id)
+
+local entities_all = EntityGetInRadius(x, y, radius)
+local count = #entities_all
+
+if count < max then
+	SetRandomSeed(x, y + count)
+	local file = files[Random(1, #files)]
+	EntityLoad(file, x + Random(-10, 10), y + Random(-5, 0))
+end
+
+EntityKill(entity_id)
