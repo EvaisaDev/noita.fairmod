f0d278e7bc50a682613e33ce330fdc5cb70987e2
Lamia <xaxnas@gmail.com>
2025-03-16 12:30:13 +0000

    combined water tweaks with worse materials (less parsing)

--- files/content/worse_materials/init.lua
+++ files/content/worse_materials/init.lua
@@ -1,7 +1,16 @@
 local nxml = dofile_once("mods/noita.fairmod/files/lib/nxml.lua") --- @type nxml
 
+local water_to_fuckup = {
+	water = true,
+	water_static = true,
+	water_ice = true,
+	water_swamp = true,
+	water_salt = true,
+	swamp = true,
+}
+
 local modifications = {
-	["magic_liquid_protection_all"] = function(celldata)
+	magic_liquid_protection_all = function(celldata)
 		celldata:set("status_effects", "PROTECTION_NONE")
 	end,
 }
@@ -10,6 +19,21 @@ for materials in nxml.edit_file("data/materials.xml") do
 	for celldata in materials:each_of("CellData") do
 		local name = celldata.attr.name
 		if modifications[name] then modifications[name](celldata) end
+	end
+
+	for reaction in materials:each_of("Reaction") do
+		local attr = reaction.attr
+		local input1, input2 = attr.input_cell1, attr.input_cell2
+
+		if input2 == "radioactive_liquid" and water_to_fuckup[input1] then
+			attr.output_cell1, attr.output_cell2 = "cement", "cement"
+		elseif water_to_fuckup[input2] then
+			if input1 == "[lava]" then
+				attr.output_cell1, attr.output_cell2 = "gunpowder_unstable", "rainbow_gas"
+			elseif input1 == "magic_liquid_mana_regeneration" then
+				attr.output_cell1 = "lava"
+			end
+		end
 	end
 end
 
