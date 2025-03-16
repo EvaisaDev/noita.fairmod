765d2dcddf69dc907b1de5e0cd868973ff112bd4
Adam Heinermann <aheinerm@gmail.com>
2025-03-16 14:32:44 -0700

    Extend and randomize milk biome

--- files/content/milk_biome/scripts/biome_map_appends.lua
+++ files/content/milk_biome/scripts/biome_map_appends.lua
@@ -1,19 +1,30 @@
 local color = 0xff83cd35
 
 local positions = {
+	{ 41, 24 },
+	-- { 42, 24 }, -- cauldron entrance
 	{ 42, 25 },
 	{ 42, 26 },
 	{ 42, 27 },
+	{ 43, 24 },
+	{ 43, 25 },
+	{ 43, 26 },
 	{ 43, 27 },
 	{ 43, 28 },
+	{ 44, 27 },
+	{ 45, 27 },
+	{ 45, 28 },
+	{ 46, 27 },
+	{ 46, 28 },
+	{ 47, 27 },
+	{ 45, 25 },
+	{ 46, 25 },
+	{ 47, 25 },
+	{ 45, 26 },
+	{ 46, 26 },
+	{ 47, 26 },
 }
 
 for _, position in ipairs(positions) do
 	BiomeMapSetPixel(position[1], position[2], color)
 end
-
--- for x=1,size_x do
---     for y=1,size_y do
---         BiomeMapSetPixel( x + pos_x - 1, y + pos_y - 1, color )
---     end
--- end
