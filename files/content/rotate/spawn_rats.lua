1fc14ef3eda4722268ca20262fd62226eee8fec3
Lamia <xaxnas@gmail.com>
2025-03-16 15:41:31 +0000

    camerabound for rats

--- files/content/rotate/spawn_rats.lua
+++ files/content/rotate/spawn_rats.lua
@@ -1,4 +1,4 @@
-EntityLoad("mods/noita.fairmod/files/content/rotate/rotta-cart.xml", 470, -120)
-EntityLoad("mods/noita.fairmod/files/content/rotate/mother_rotta.xml", 500, -118)
-EntityLoad("mods/noita.fairmod/files/content/rotate/rotta.xml", 435, -109)
-EntityLoad("mods/noita.fairmod/files/content/rotate/rat_hole.xml", 456, -107)
+EntityLoadCameraBound("mods/noita.fairmod/files/content/rotate/rotta-cart.xml", 470, -120)
+EntityLoadCameraBound("mods/noita.fairmod/files/content/rotate/mother_rotta.xml", 500, -118)
+EntityLoadCameraBound("mods/noita.fairmod/files/content/rotate/rotta.xml", 435, -109)
+EntityLoadCameraBound("mods/noita.fairmod/files/content/rotate/rat_hole.xml", 456, -107)
