8d6a559d40ec24bd9f388329159c4ea9bc6c9863
Lamia <xaxnas@gmail.com>
2025-03-23 20:17:20 +0000

    eviler egg

--- files/content/pixelscenes/biomes/dragoncave/entity/wiggle.lua
+++ files/content/pixelscenes/biomes/dragoncave/entity/wiggle.lua
@@ -1,3 +1,6 @@
 local entity_id = GetUpdatedEntityID()
+local x, y = EntityGetTransform(entity_id)
 
-PhysicsApplyForce(entity_id, 2000000, 2000000)
+SetRandomSeed(x, y + GameGetFrameNum())
+
+PhysicsApplyForce(entity_id, 2000000 * Random(-8, 8), 2000000 * Random(-8, 8))
