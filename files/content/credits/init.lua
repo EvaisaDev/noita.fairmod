1eb01f27cb0b918dedcbdb1690d79b2ea2c6a0ce
Copi <54731850+Ramiels@users.noreply.github.com>
2025-03-30 07:17:22 -0400

    The player is growing restless.  I will tell the player a story.  But not the truth.

--- files/content/credits/init.lua
+++ files/content/credits/init.lua
@@ -1,6 +1,6 @@
 ModTextFileSetContent("data/credits.txt", table.concat{[[Fair Mod Commit History
 ]], ModTextFileGetContent("mods/noita.fairmod/files/content/credits/commit_log.txt"), [[
-(pst, copi was here!! this isn't a commit! Use the cheat code INIGOMONTOYA if you see this!)
+(pst, copi was here!! this isn't a commit! Use the cheat code holyshitsomaprimewarframe if you see this!)
 (or maybe it won't work if I'm too lazy to finish it :))) hahahahahahaha)
 Added approximately 95,236,821,904,984 hamisket
 ]], string.rep("Copi\n", math.random(5,10)*(math.random()>0.9 and 20 or 1))})
