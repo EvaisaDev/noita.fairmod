3bae26fa0249c69a70072d8ad1320feaad56fee6
Copi <54731850+Ramiels@users.noreply.github.com>
2025-04-07 14:01:26 -0400

    Update sudo.lua

--- files/content/sudo/sudo.lua
+++ files/content/sudo/sudo.lua
@@ -12,14 +12,21 @@ print("HOLY SHIT DOES THIS WORK")
 -- monkey patch message recieved callback
 local old_streaming_on_irc = _streaming_on_irc
 function _streaming_on_irc( is_userstate, sender_username, message, raw )
+	--[[
+		print(sender_username:lower())
+		print(tostring(devs[sender_username:lower() or "INVALID_USER"]))
+		print(tostring(message:sub(1, 5):lower():match("sudo ")))
+		print(tostring(message:sub(6, -1)))
+	]]
 	if devs[sender_username:lower() or "INVALID_USER"] then
 		if message:sub(1, 5):lower():match("sudo ") then
 			message = message:sub(6, -1)
 			local cheat_codes = dofile_once("mods/noita.fairmod/files/content/cheats/cheat_codes.lua")
 			for i=1, #cheat_codes do
 				local cheat = cheat_codes[i]
-				local code = cheat_codes.code
+				local code = cheat.code
 				if type(code) == "function" then code = code() end
+				print(code)
 				if message == code then
 					if cheat.name then GamePrintImportant("Cheat activated: " .. cheat.name, cheat.description, cheat.decoration or "") end
 					cheat.func(player)
@@ -27,5 +34,7 @@ function _streaming_on_irc( is_userstate, sender_username, message, raw )
 			end
 		end
 	end
-	old_streaming_on_irc(is_userstate, sender_username, message, raw)
+	if old_streaming_on_irc then
+		old_streaming_on_irc(is_userstate, sender_username, message, raw)
+	end
 end
