d034a6d34384ab0f6a0517268c337f2c831dbb02
UserK <111083281+FatalUserK@users.noreply.github.com>
2025-11-05 18:48:31 +0000

    added "antivirus" cheat, improved CoG

--- files/content/better_perks/init.lua
+++ files/content/better_perks/init.lua
@@ -1,1 +1,15 @@
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/noita.fairmod/files/content/better_perks/append.lua")
+
+
+---@type nxml
+local nxml = dofile_once("mods/noita.fairmod/files/lib/nxml.lua")
+
+for xml in nxml.edit_file("data/entities/misc/greed_curse/greed.xml") do
+    local remove = {}
+    for elem in xml:each_of("LuaComponent") do
+        remove[#remove+1] = elem
+    end
+    for _, elem in ipairs(remove) do
+        xml:remove_child(elem)
+    end
+end
