12086cda680ce88d8f48c2d67eab42828dc26465
Evaisa <evagiacosa1@gmail.com>
2025-11-30 16:01:36 +0100

    Update perk_list.lua

--- files/content/tmtrainer/files/scripts/append/perk_list.lua
+++ files/content/tmtrainer/files/scripts/append/perk_list.lua
@@ -29,9 +29,20 @@ local function generate_icon(index, original_icon, is_first, icon_type)
 	local icon_dir = "mods/noita.fairmod/files/content/tmtrainer/files/perk_icons/"
 	local new_icon_path = icon_dir .. icon_type .. "_" .. index .. ".png"
 
-	-- Get image IDs and dimensions
-	local original_icon_id, original_icon_width, original_icon_height = ModImageIdFromFilename(original_icon)
-	local new_icon_id, new_icon_width, new_icon_height = ModImageIdFromFilename(new_icon_path)
+	local original_icon_id, original_icon_width, original_icon_height
+	local new_icon_id, new_icon_width, new_icon_height
+
+	if(ModImageWhoSetContent(original_icon) == "" )then
+		original_icon_id, original_icon_width, original_icon_height = ModImageMakeEditable(original_icon, 0, 0)
+	else
+		original_icon_id, original_icon_width, original_icon_height = ModImageIdFromFilename( original_icon )
+	end
+
+	if(ModImageWhoSetContent(new_icon_path) == "" )then
+		new_icon_id, new_icon_width, new_icon_height = ModImageMakeEditable(new_icon_path, original_icon_width, original_icon_height)
+	else
+		new_icon_id, new_icon_width, new_icon_height = ModImageIdFromFilename( new_icon_path )
+	end
 
 	if is_first then
 		-- Copy the entire original icon onto the new icon
