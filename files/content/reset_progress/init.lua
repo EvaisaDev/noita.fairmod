13c4ce823b55ca23cbf67882f93e343ffe33c7fe
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-02-09 05:01:57 +0000

    reworked reset_progress structure

--- files/content/reset_progress/init.lua
+++ files/content/reset_progress/init.lua
@@ -7,14 +7,27 @@ for i = 1, #achievements do
 	RemoveFlagPersistent(achievement.flag)
 end
 
-RemoveFlagPersistent("fairmod_dmca_warning_shown")
-RemoveFlagPersistent("fairmod_spawned_superchest")
-RemoveFlagPersistent("fairmod_noitillionare_winner")
-RemoveFlagPersistent("crashed_by_wizard")
-ModSettingRemove("fairmod_win_count")
-ModSettingRemove("fairmod.death_locations")
-ModSettingRemove("fairmod.radios_activated_highscore")
-ModSettingRemove("noita.fairmod.reset_progress")
-RemoveFlagPersistent("fairmod_touched_minecart_trigger")
-ModSettingRemove("fairmod.deaths")
-ModSettingRemove("noita.fairmod.cpand_tmtrainer_chance")
+local settings = {
+	"fairmod_win_count",
+	"fairmod.death_locations",
+	"fairmod.radios_activated_highscore",
+	"noita.fairmod.reset_progress",
+	"fairmod.deaths",
+	"noita.fairmod.cpand_tmtrainer_chance",
+}
+
+local persistent_flags = {
+	"fairmod_dmca_warning_shown",
+	"fairmod_spawned_superchest",
+	"fairmod_noitillionare_winner",
+	"crashed_by_wizard",
+	"fairmod_touched_minecart_trigger",
+}
+
+for _,setting in ipairs(settings) do
+	ModSettingRemove(setting)
+end
+
+for _,flag in ipairs(persistent_flags) do
+	RemoveFlagPersistent(flag)
+end
