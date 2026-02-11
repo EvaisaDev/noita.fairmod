b53ac187ac88d1d3aa03d379ef5931725b0da885
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-02-11 07:29:41 +0000

    reworked credits stuff

--- files/content/credits/init.lua
+++ files/content/credits/init.lua
@@ -1,6 +1,23 @@
-ModTextFileSetContent("data/credits.txt", table.concat{[[Fair Mod Commit History
-]], ModTextFileGetContent("mods/noita.fairmod/files/content/credits/commit_log.txt"), [[
-(pst, copi was here!! this isn't a commit! Use the cheat code holyshitsomaprimewarframe if you see this!)
-(or maybe it won't work if I'm too lazy to finish it :))) hahahahahahaha)
-Added approximately 95,236,821,904,984 hamisket
-]], string.rep("Copi\n", math.random(5,10)*(math.random()>0.9 and 20 or 1))})
+local credits = "\n \n=== Fair Mod Commit History ===\n" .. ModTextFileGetContent("mods/noita.fairmod/files/content/credits/commit_log.txt"):gsub("\r\n", "\n")
+
+local function escape(str) return str:gsub("[%(%)%.%%%+%-%*%?%[%^%$%]]", "%%%1") end
+
+local function add_release_break(target, release)
+    target = "\n" .. target .. "\n"
+    credits = credits:gsub(escape(target), target .. " \n--- " .. release .. " ---\n \n")
+end
+
+add_release_break("fixed init (was in absolute fucking shambles)", " MAIN RELEASE 31/10/24")
+add_release_break("Labor of Loathing is here :)", "LABOR OF LOATHING 1/4/25")
+
+local dev_messages = {}
+
+local dev_msg_txt = #dev_messages ~= 0 and "\n \n \n==== FINAL WORDS ====" or ""
+
+for dev, message in pairs(dev_messages) do
+    dev_msg_txt = dev_msg_txt .. "~MESSAGES FROM " .. dev .. "~\n" .. message .. "\n \n"
+end
+
+credits = credits .. dev_msg_txt
+
+ModTextFileSetContent("data/credits.txt", credits)
